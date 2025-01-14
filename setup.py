31504b15c3ea0e124efd7a81befd6fc33334607b
Rushikesh0912 <rushikeshgaikhe09@gmail.com>
2025-01-14 15:41:43 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'rushikesh gaikhe',
+    author_email= 'rushikeshgaikhe09@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
