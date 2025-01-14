2b5f5fccfc61c86f804098e462bb038123cc690c
Rushikesh0912 <rushikeshgaikhe09@gmail.com>
2025-01-14 19:26:56 +0530

    utils and logger

---
+++ cellSegmentation/utils/main_utils.py
@@ -0,0 +1,35 @@
+import os.path
+import sys
+import yaml
+import base64
+
+from cellSegmentation.exception import AppException
+from cellSegmentation.logger import logging
+
+
+def read_yaml_file(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            logging.info("Read yaml file successfully")
+            return yaml.safe_load(yaml_file)
+
+    except Exception as e:
+        raise AppException(e, sys) from e
+    
+
+
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open("./data/" + fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
+
+    
+    
